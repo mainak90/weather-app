09669d703db298e1bd05120c4c7ba3c7d99a6a21
MAINAK Dhar (ITS/DCI) <dhar.mainak.ext@proximus.com>
2017-07-17 21:58:44 +0200

    new commit

---
+++ personal/weather-app/app.js
@@ -0,0 +1,27 @@
+const yargs = require('yargs');
+const geocode = require('./geocode/geocode.js');
+
+
+const argv = yargs
+    .options({
+        a: {
+            demand: true,
+            alias: 'address',
+            describe: 'Address to fetch weather for',
+            string: true
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+//console.log(argv);
+
+
+geocode.geocodeAddress(argv.address,(err, results) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(JSON.stringify(results, undefined, 2));
+    }
+});
